8355df9c7367e590cc96632f5939367ecaeca4c2
avezeteu <avezeteu@adobe.com>
2022-11-27 20:22:02 +0200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+import ReactDOM from "react-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { ChakraProvider, ColorModeScript, HStack, Spinner, theme, VStack } from '@chakra-ui/react'
+import HomePage from './pages/HomePage/HomePage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ColorModeScript />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        {/* <Layout> */}
+          <React.Suspense fallback={<VStack h='100vh' justify='center' align='center'><HStack justify='center' align='center'><Spinner thickness='4px' emptyColor='gray.200' size='xl'/></HStack></VStack>}>
+            <Routes>
+              <Route index element={<HomePage />} />
+            </Routes>
+          </React.Suspense>
+        {/* </Layout> */}
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import react, { useState } from "react"
+
+
+
+const HomePage = () => {
+  const [loadingItems, setLoadingItems] = useState(true);
+
+    return (
+        <div>Home</div>
+    )
+}
+
+export default HomePage
